2dc57edac29301f37453b629e5a2c014cb8d1250
Arnaud <peyrachearnaud@gmail.com>
2022-06-16 16:26:58 +0200

    search new

--- js/search.js
+++ js/search.js
@@ -1,5 +1,5 @@
 function generateCardInfoEvent(infos){ //[id_match,titre,sport,ville,date,heure,inscrits,max]
-    let txt = '<div class="card-event">'
+    let txt = '<div class="card-event" id="card-event-number' + infos[0] + '">'
         + '<div class="card-event-infos-background">'
         + '<div class="card-event-infos">'
         + infos[1] + ', ' + infos[2] + '<br>'
@@ -20,15 +20,55 @@ function generateMyEvent(infos){ //[titre]
     let txt = '<div class="one-event">'
         + '<span>' + infos[0] + '</span>'
         + '</div>';
-    $('#me-games-all').append(txt);
+    $('#my-games-all').append(txt);
+}
+
+function setSearchMode() {
+    let txt = "<div id = 'result'>" +
+        "</div>" +
+        "<h1 id=\"my-events-title\">Mes matchs :</h1>" +
+        "<div id=\"my-events\">" +
+
+        "<div id=\"me-organize\" class=\"under-block\">" +
+        "<h2>Ceux que j'organise :</h2>" +
+        "<div id=\"me-organize-all\">" +
+
+        "</div>" +
+        "<button class=\"classic-button\">Nouveau</button>" +
+        "</div>" +
+        "<div id=\"my-games\" class=\"under-block\">" +
+        "<h2>Ceux qui me concernent :</h2>" +
+        "<div id=\"my-games-all\">" +
+
+        "</div>" +
+        "<button class=\"classic-button hidden\">Nouveau</button>" +
+        "</div>" +
+        "</div>";
+    $('#page').html(txt);
+}
+
+function setShowInfosMode(){
+
 }
 
 $(document).ready(function(){
+
+
+
+    //setSearchMode();
     for (let i = 0; i < 15; i++){
         generateCardInfoEvent([i,'test','foot-ball','Nantes','10-10-2022','12:00',1,12]);
     }
+
     generateMyOrganizeEvent(['foot2rue']);
     generateMyEvent(['foot2rue']);
+
+    $('.card-event').click(function (e)
+        {
+            console.log(e.currentTarget.id);
+
+        }
+    );
 });
 
 
