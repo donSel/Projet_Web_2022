ab690d8a71a960772136eb1b8da0767034e7155b
Mickaël Neroda <mickael.neroda@isen-ouest.yncrea.fr>
2022-06-22 17:02:34 +0200

    .

--- php/fill_database.php
+++ php/fill_database.php
@@ -44,9 +44,17 @@
     $number_min_player = 2;
     $arrDate = [];
     $arrHour = [];
-    $arrAdresse = ['1 Pl. Alexis-Ricordeau', '12 boulevard de Alsace', '3 place Anatole France']; 
+    $arrTown = ['Nantes', 'Vaulx-en-velin', 'Angers', 'Paris', ];
+    $arrAddress = ['1 Pl. Alexis-Ricordeau', '12 boulevard de Alsace', '3 place Anatole France', 'stade de ']; 
     $price = "5";
     $arrTitle = [];
+    $age_range = "14-30";
+    $match_description = "Evenements sportif de dingue, n'hésitez pas à venir les places sont limitées";
+    $duration = "01:30:00";
+    $organizer_id = "mickael.neroda@mail.com";
+    $arrSport = [];
+    
+    
     
     $arrLastName = ['ROM-PUISAIS', 'ROM DANE', 'ROLZOU', 'ROLZHAUSEN', 'ROLZEN'	, 'ROLZ', 'ROLU', 'ROLTMIT'	, 'ROLTHMEIR', 'ROLT-LEVEQUE', 'ROBICOUET', 'ROBICO', 'ROBICKEZ', 'ROBICHOY', 'ROBICHOU', 'RIESSE'];
     $arrFirstName = ['Bernard', 'Thomas' , 'Petit', 'Robert', 'Richard', 'Durand', 'Dubois', 'Moreau', 'Laurent', 'Simon', 'Michel', 'Lefebvre', 'Leroy', 'Roux', 'David', 'Bertrand'];
@@ -55,7 +63,6 @@
     
     $photo_url = '';
     $mdp = 'aze';
-    $arrVille = ['Nantes', 'Vaulx-en-velin', 'Angers', 'Paris', ];
     $arrVilleLen = count($arrVille);
     
     for ($i = 0; $i < $arrFirstNameLen; $i++){
@@ -69,4 +76,19 @@
             echo "player not inserted !<br>";
         }
     }
+    
+    
+//------------------------------------------------------------
+//-- Filling Play
+//------------------------------------------------------------
+    
+    
+//------------------------------------------------------------
+//-- Filling Score
+//------------------------------------------------------------
+    
+    
+//------------------------------------------------------------
+//-- Filling Match Result
+//------------------------------------------------------------
 ?>
--- sql/model.sql
+++ sql/model.sql
@@ -85,7 +85,7 @@ CREATE TABLE public.match(
 	registered_count        INT  NOT NULL ,
 	title                   VARCHAR (50) NOT NULL ,
 	age_range               VARCHAR (50) NOT NULL , -- ignore it
-	match_description       VARCHAR (50) NOT NULL ,
+	match_description       VARCHAR (200) NOT NULL ,
 	duration                TIME  NOT NULL ,
 	organizer_id            VARCHAR (50) NOT NULL ,
 	sport_id                INT  NOT NULL ,
